a5f293c1d351bef2b6c20163036381219f30bd1f
Prof John David Zehnpfennig II, PE <111508888+prof-jz@users.noreply.github.com>
2023-01-17 20:53:46 +0000

    Initial state for Lesson 3 - code from text's Chapter 1 area.cpp augmented with several exercises outlined in comments.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,74 @@
+// Program Area calculates the area of a square.     
+// The user is prompted to enter the number of inches on each
+// side. Note that “endl” in line 7 ends in the letter “l”, not
+// the number one.
+
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!\n";
-}
+using namespace std;                                                            
+int main ()
+{
+  int  inches;      //try working with various data types, changing from 'int' to the other numeric types
+
+  cout  << "Enter the number of inches on a side "
+        << endl;
+  cout  << "Press the return key."
+        << endl;
+  cin  >> inches;
+  cout  << endl  
+        << "The area is "  << inches * inches  <<"."
+        << endl;
+  
+/**********************************
+* Create output for: Parameter  
+* Variables: int inch
+* Formula: 2 * h + 2 * w 
+**********************************/  
+
+
+/**********************************
+* Create output for: Volume
+* Variables: int inch
+* Formula: h * l * w
+**********************************/
+
+
+/**********************************
+* Create input for: Number of squares
+* Remember to declare a new variable!
+* Variables: NumSquares
+**********************************/
+
+
+/**********************************
+* Create output for: Total Surface Area
+* Variables: int inch, NumSquares
+* Formula: n * (2 * h + 2 * w)
+**********************************/
+
+  
+
+/**********************************
+* Assume 'inches' is a radius of a circle
+* Create output for: Circumference
+* Variables: int inch
+* Formula: 2 * pi * r
+* Headers: iostream, math.h
+**********************************/  
+
+
+  
+/**********************************
+* Create output for: Order of Operations
+* Variables:  --none--
+
+* Write the below formula in a cout statement. 
+* Before running, write it on paper and calculate the expected result.
+
+* Formula: ( 2 + 2 * ( 1 - 3)^2  )/ (8 + (-5)) - 1
+**********************************/  
+
+  
+return 0;
+  }
+
